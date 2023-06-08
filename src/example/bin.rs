31d93b934b6d357f32dc77bc1dfd64fe981be329
Etho <58352929+EthoIRL@users.noreply.github.com>
2023-06-07 19:49:33 -0700

    Updated example project

--- src/example/bin.rs
+++ src/example/bin.rs
@@ -1,13 +1,19 @@
-use std::thread;
+use std::{io, ops::Deref, thread};
 
-use hid_api_rs::{gadgets::mouse, HidSpecification};
+use hid_api_rs::{
+    gadgets::{
+        keyboard::{self, KeyCodeModifier, UsbKeyCode},
+        mouse,
+    },
+    HidSpecification,
+};
 
 extern crate hid_api_rs;
 
 pub fn main() {
     let specification: HidSpecification = HidSpecification {
         mouse_inputs: Some([String::from("/dev/input/mice")].to_vec()),
-        keyboard_inputs: None,
+        keyboard_inputs: Some([String::from("/dev/input/by-id/usb-Keychron_K4_Keychron_K4-event-kbd"), String::from("/dev/input/by-id/usb-Logitech_G413_Silver_Mechanical_Gaming_Keyboard_1172384C3230-event-kbd")].to_vec()),
         gadget_output: String::from("/dev/hidg0"),
     };
 
@@ -17,50 +23,90 @@ pub fn main() {
         };
     });
 
-    let mut switcher: bool = false;
+    thread::spawn(|| {
+        let mut switcher: bool = false;
+        let mut key_switch: bool = false;
+
+        loop {
+            let keyboard_state = hid_api_rs::get_keyboard();
+
+            // if let Ok(ref modifier_rwl) = keyboard_state.modifier.try_read() {
+            //     if let Some(modifier) = modifier_rwl.deref() {
+            //         println!("Modifier: {}", modifier.to_string());
+            //     }
+            // }
+
+            // if let Ok(ref keys_down) = keyboard_state.keys_down.try_read() {
+            //     for key_down in keys_down.to_vec() {
+            //         if let Ok(key) = UsbKeyCode::try_from(key_down as i16) {
+            //             println!("Key Down: {}", key.to_string())
+            //         };
+            //     }
+            // }
+
+            if keyboard::is_key_down(UsbKeyCode::KEYY, keyboard_state) {
+                key_switch = !key_switch;
+            }
+
+            // println!("Total mouses in circulation: {}",h id_api_rs::get_mouses().len());
+            for mouse in hid_api_rs::get_mouses() {
+                if let Ok(left_button) = mouse.mouse_state.left_button.try_read() {
+                    if *left_button == true {
+                        drop(left_button);
+                        
+                        println!("left: True");
+                        println!("Key Switch: {}", key_switch)
+                    }
+                }
+
+                if let Ok(middle_button) = mouse.mouse_state.middle_button.try_read() {
+                    if *middle_button == true {
+                        println!("Inverting!");
+                        if switcher == false {
+                            if let Ok(mut invert_x) = mouse.mouse_state.invert_x.try_write() {
+                                *invert_x = true;
+                            }
+
+                            if let Ok(mut invert_y) = mouse.mouse_state.invert_y.try_write() {
+                                *invert_y = true;
+                            }
+
+                            if let Ok(mut invert_wheel) = mouse.mouse_state.invert_wheel.try_write() {
+                                *invert_wheel = true;
+                            }
+                        } else {
+                            if let Ok(mut invert_x) = mouse.mouse_state.invert_x.try_write() {
+                                *invert_x = false;
+                            }
+
+                            if let Ok(mut invert_y) = mouse.mouse_state.invert_y.try_write() {
+                                *invert_y = false;
+                            }
+
+                            if let Ok(mut invert_wheel) = mouse.mouse_state.invert_wheel.try_write() {
+                                *invert_wheel = false;
+                            }
+                        }
+                        switcher = !switcher;
+                    }
+                }
+                // println!("Left: {}, Right: {}, Middle: {}", mouses.mouse_state.left_button.read()., mouses.mouse_state.right_button.read().unwrap(), mouses.mouse_state.middle_button.read().unwrap());
+            }
+        }
+    });
 
     loop {
-        // println!("Total mouses in circulation: {}",h id_api_rs::get_mouses().len());
-        for mouse in hid_api_rs::get_mouses() {
-            if let Ok(left_button) = mouse.mouse_state.left_button.try_read() {
-                if *left_button == true {
-                    drop(left_button);
-                    println!("left: True");
-                }
-            }
+        let mut answer = String::new();
 
-            if let Ok(middle_button) = mouse.mouse_state.middle_button.try_read() {
-                if *middle_button == true {
-                    println!("Inverting!");
-                    if switcher == false {
-                        if let Ok(mut invert_x) = mouse.mouse_state.invert_x.try_write() {
-                            *invert_x = true;
-                        }
+        io::stdin()
+            .read_line(&mut answer)
+            .ok()
+            .expect("Failed to read line");
+        if !answer.is_empty() {
+            println!("Stopping");
+            hid_api_rs::stop_passthrough();
 
-                        if let Ok(mut invert_y) = mouse.mouse_state.invert_y.try_write() {
-                            *invert_y = true;
-                        }
-
-                        if let Ok(mut invert_wheel) = mouse.mouse_state.invert_wheel.try_write() {
-                            *invert_wheel = true;
-                        }
-                    } else {
-                        if let Ok(mut invert_x) = mouse.mouse_state.invert_x.try_write() {
-                            *invert_x = false;
-                        }
-
-                        if let Ok(mut invert_y) = mouse.mouse_state.invert_y.try_write() {
-                            *invert_y = false;
-                        }
-
-                        if let Ok(mut invert_wheel) = mouse.mouse_state.invert_wheel.try_write() {
-                            *invert_wheel = false;
-                        }
-                    }
-                    switcher = !switcher;
-                }
-            }
-            // println!("Left: {}, Right: {}, Middle: {}", mouses.mouse_state.left_button.read()., mouses.mouse_state.right_button.read().unwrap(), mouses.mouse_state.middle_button.read().unwrap());
+            break;
         }
     }
 }
