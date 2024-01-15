48a8eaacf0da7a22bbd1d06a01697c2d6c081a67
Etho <58352929+EthoIRL@users.noreply.github.com>
2024-01-14 22:45:15 -0800

    Update bin-mouse to include side buttons in example

--- src/example/bin-mouse.rs
+++ src/example/bin-mouse.rs
@@ -12,7 +12,7 @@ pub fn main() {
     env::set_var("RUST_BACKTRACE", "full");
 
     let specification: HidSpecification = HidSpecification {
-        mouse_inputs: Some([HidMouse {mouse_path: String::from("/dev/input/mice"), mouse_poll_rate: Some(1000)}].to_vec()),
+        mouse_inputs: Some([HidMouse { mouse_path: String::from("/dev/input/mice"), mouse_poll_rate: Some(1000) }].to_vec()),
         keyboard_inputs: Some([String::from("/dev/input/by-id/usb-Logitech_G502_HERO_Gaming_Mouse_0E6D395D3333-if01-event-kbd")].to_vec()),
         gadget_output: String::from("/dev/hidg0"),
     };
@@ -31,10 +31,8 @@ pub fn main() {
                 let mouse = &mut mouses[mouse_index];
 
                 if let Ok(mouse_state) = mouse.mouse_state.try_read() {
-                    println!("Left: {}, Right: {}, Middle: {}", mouse_state.left_button, mouse_state.right_button, mouse_state.middle_button);
+                    println!("Left: {}, Right: {}, Middle: {}, Side-4: {}, Side-5: {}", mouse_state.left_button, mouse_state.right_button, mouse_state.middle_button, mouse_state.four_button, mouse_state.five_button);
                 }
-
-                // println!("Left: {}, Right: {}, Middle: {}", mouse.mouse_state.left_button.try_read().unwrap(), mouse.mouse_state.right_button.try_read().unwrap(), mouse.mouse_state.middle_button.try_read().unwrap());
             }
 
             thread::sleep(Duration::from_millis(500))
